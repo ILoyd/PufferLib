df9e8528fb07d6faf23b6cc5639731c0be16abdd
IL0yd <marcell.litavecz@gmail.com>
2025-11-07 14:50:33 +0100

    Initial commit

--- pufferlib/ocean/environment.py
+++ pufferlib/ocean/environment.py
@@ -163,6 +163,7 @@ MAKE_FUNCTIONS = {
     'spaces': make_spaces,
     'multiagent': make_multiagent,
     'slimevolley': 'SlimeVolley',
+    'escape_room' : 'EscapeRoom',
 }
 
 def env_creator(name='squared', *args, **kwargs):
